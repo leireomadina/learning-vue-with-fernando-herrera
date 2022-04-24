c65d812b1bda278733118953f770bade4cb54d5b
Leire Ordeñana <leireomadina@gmail.com>
2022-04-24 12:01:14 +0200

    Add pokemon picture and options components

---
+++ 03-pokemon-game/src/components/PokemonPicture.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pokemon-container">
+    <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Pokemon">
+
+    <!-- <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Pokemon"> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+/* Pokemon Picture */
+.pokemon-container {
+  height: 200px;
+  position: relative;
+}
+
+img {
+  height: 200px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+
+.hidden-pokemon {
+  filter: brightness(0);
+}
+
+</style>>
---
+++ 03-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>¿Quién es este Pokemon?</h1>
+  <PokemonPicture />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
