c882f9d28c830e07f3e20f33779d4d109a5ab90f
Leire Ordeñana <leireomadina@gmail.com>
2022-04-24 17:31:38 +0200

    Conect with Pokemon API and finish game

--- 03-pokemon-game/src/components/PokemonPicture.vue
+++ 03-pokemon-game/src/components/PokemonPicture.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="pokemon-container">
-    <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Pokemon">
-
-    <!-- <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Pokemon"> -->
+    <img class="hidden-pokemon" 
+      :src="imgSrc" 
+      alt="Pokemon"
+    >
+    <img 
+      v-if="showPokemon"
+      class="fade-in"
+      :src="imgSrc"
+      alt="Pokemon"
+    >
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+    }
+  }
 }
 </script>
 
 <style scoped>
 
-/* Pokemon Picture */
 .pokemon-container {
   height: 200px;
   position: relative;
--- 03-pokemon-game/src/pages/PokemonPage.vue
+++ 03-pokemon-game/src/pages/PokemonPage.vue
@@ -1,17 +1,75 @@
 <template>
-  <h1>¿Quién es este Pokemon?</h1>
-  <PokemonPicture />
-  <PokemonOptions />
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+      <h1>¿Quién es este Pokemon?</h1>
+    <PokemonPicture 
+      :pokemon-id="pokemon.id" 
+      :show-pokemon="showPokemon" 
+    />
+    <PokemonOptions 
+      :pokemons="pokemonArr" 
+      @selection-pokemon="checkAnswer"
+    />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo juego</button>
+    </template>
+    
+  </div>
 </template>
 
 <script>
 import PokemonPicture from '@/components/PokemonPicture'
 import PokemonOptions from '@/components/PokemonOptions'
 
+import getPokemonOptions from '../helpers/getPokemonOptions'
+
 export default {
   components: {
     PokemonPicture,
     PokemonOptions
+  },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
+    }
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions()
+      // console.log(this.pokemonArr)
+
+      const randomInt = Math.floor( Math.random() * 4 )
+      this.pokemon = this.pokemonArr[ randomInt ]
+    },
+    checkAnswer( selectedId ) {
+      // console.log('Pokemon page llamado', selectedId);
+      this.showPokemon = true
+      this.showAnswer = true
+
+      if ( selectedId === this.pokemon.id ) {
+        this.message = `Correcto, es ${this.pokemon.name}`
+      } else {
+        this.message = `Oops, era ${this.pokemon.name}`
+      }
+    },
+    newGame() {
+      this.showPokemon = false
+      this.showAnswer = false
+      this.pokemonArr = []
+      this.pokemon = null
+
+      this.mixPokemonArray()
+    }
+  },
+  mounted() {
+    this.mixPokemonArray()
   }
 }
+
 </script>
