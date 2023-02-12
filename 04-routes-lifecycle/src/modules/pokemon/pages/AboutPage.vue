e7a3d185e44976d9e008d65f12ef0463e9770e50
leireomadina <leireomadina@gmail.com>
2023-02-12 12:44:59 +0100

    Add about page and test lifecycle hooks

--- 04-routes-lifecycle/src/modules/pokemon/pages/AboutPage.vue
+++ 04-routes-lifecycle/src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,61 @@
 <template>
 	<h1>About page</h1>
+	<p>{{ name }}</p>
+	<button @click="onChangeName">Change name</button>
 </template>
+
+<script>
+export default {
+
+	data() {
+		return {
+			name: 'Leire'
+		}
+	},
+	methods: {
+		onChangeName() {
+			this.name = 'Risingdana'
+		}
+	},
+	// Lifecycle hooks
+	beforeCreate() {
+		console.log('beforeCreate')
+	},
+	created() {
+		console.log('created')
+	},
+	beforeMount() {
+		console.log('beforeMount')
+	},
+	mounted() {
+		console.log('mounted')
+	},
+	beforeUpdate() {
+		console.log('beforeUpdate')
+	},
+	updated() {
+		console.log('updated')
+	},
+	activated() {
+		console.log('activated')
+	},
+	deactivated() {
+		console.log('deactivated')
+	},
+	beforeUnmount() {
+		console.log('beforeUnmount')
+	},
+	unmounted() {
+		console.log('unmounted')
+	},
+	errorCaptured() {
+		console.log('errorCaptured')
+	},
+	renderTracked() {
+		console.log('renderTracked')
+	},
+	renderTriggered() {
+		console.log('renderTriggered')
+	},
+}
+</script>
