6c0d79c5568af5cb35e39ea02499e77f29719fa8
leireomadina <leireomadina@gmail.com>
2023-02-12 12:44:00 +0100

    Add navbar component

---
+++ 04-routes-lifecycle/src/modules/shared/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+	<nav>
+		<router-link :to="{ name: 'home' }">Pokemon List</router-link>
+		<router-link :to="{ name: 'pokemon-id', params: {id: 100 } }">Pokemon per id</router-link>
+		<router-link :to="{ name: 'about' }">About</router-link>
+	</nav>
+</template>
+
+<style scoped>
+nav {
+	max-width: 20rem;
+	margin: 2rem auto;
+	display: flex;
+	justify-content: space-between;
+}
+	nav a {
+		color: #35495e;
+	}
+		nav a.router-link-exact-active {
+			color: #41b883;
+		}
+</style>
