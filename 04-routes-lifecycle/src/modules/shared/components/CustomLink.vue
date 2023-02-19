4894a8319aba76a04a14e7dd382b9ab2f814c1dc
leireomadina <leireomadina@gmail.com>
2023-02-19 12:44:23 +0100

    Create custom link component

---
+++ 04-routes-lifecycle/src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,47 @@
+<template>
+	<a 
+		v-if="isExternalLink"
+		target="_blank"
+		:href="link.to"
+		class="normal-link"
+	>
+		{{ link.name }}
+	</a>
+	<router-link
+		v-else
+		:to="link.to"
+		v-slot="{ href, isActive }"
+	>
+		<a
+			href="href"
+			:class="isActive ? 'is-active' : 'normal-link'"
+		>
+			{{ link.name }}
+		</a>
+	</router-link>
+</template>
+
+<script>
+export default {
+	props: {
+		link: {
+			type: Object,
+			required: true
+		}
+	},
+	computed: {
+		isExternalLink() {
+			return this.link.to.startsWith('http')
+		}
+	}
+}
+</script>
+
+<style scoped>
+.is-active {
+	color: #41b883;
+}
+.normal-link {
+	color: #575757;
+}
+</style>
--- 04-routes-lifecycle/src/modules/shared/components/Navbar.vue
+++ 04-routes-lifecycle/src/modules/shared/components/Navbar.vue
@@ -1,10 +1,35 @@
 <template>
 	<nav>
-		<router-link :to="{ name: 'home' }">Pokemon List</router-link>
+		<!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
 		<router-link :to="{ name: 'pokemon-id', params: {id: 100 } }">Pokemon per id</router-link>
-		<router-link :to="{ name: 'about' }">About</router-link>
+		<router-link :to="{ name: 'about' }">About</router-link> -->
+		<CustomLink 
+			v-for="link in links"
+			:key="link.to"
+			:link="link"
+		/>
 	</nav>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue';
+
+export default {
+	data() {
+		return {
+			links: [
+				{ to: '/home', name: 'Pokemons' },
+				{ to: '/pokemonid/100', name: 'Por ID' },
+				{ to: '/about', name: 'About' },
+				{ to: 'https://google.com', name: 'Google' },
+			]
+		}
+	},
+	components: {
+		CustomLink: defineAsyncComponent( () => import('./CustomLink.vue') )
+	}
+}
+</script>
 
 <style scoped>
 nav {
@@ -13,10 +38,10 @@ nav {
 	display: flex;
 	justify-content: space-between;
 }
-	nav a {
+	/* nav a {
 		color: #35495e;
-	}
-		nav a.router-link-exact-active {
+	} */
+		/* nav a.router-link-exact-active {
 			color: #41b883;
-		}
+		} */
 </style>
